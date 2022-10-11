4209b2846f8feab60fbdf261a12ef9e67dd3134c
Omarabdullah99 <omarabdullah917303@gmail.com>
2022-10-11 19:08:06 +0600

    responsive

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -9,18 +9,18 @@ const Statistics = () => {
   const data = loaderdata.data;
   console.log(data)
     return (
-        <div className='w-2/4 mx-auto mt-10'>
-        
+      <div className=' h-64 w-64 lg:h-96 lg:w-96 mx-auto mt-10'>
+       <ResponsiveContainer width="100%" height="100%">
        <BarChart width={500} height={400} data={data}>
        <Bar dataKey="total" fill="#8884d8" />
        <XAxis dataKey="name"></XAxis>
        <YAxis></YAxis>
        <Tooltip></Tooltip>
      </BarChart>
-        
-        
-      
+       </ResponsiveContainer>
+            
         </div>
+
     );
 };
 
